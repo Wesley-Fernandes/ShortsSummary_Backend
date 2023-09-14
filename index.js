b0acd911573c87a53ccefafebb67db17b2aaee44
Wesley Fernandes <wesleyintelligence@protonmail.com>
2023-09-13 22:02:01 -0300

    cors update

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ export const openai = new OpenAI(({
 
 const io = new Server(server, {
   cors: {
-    origin: "https://shorts-summary.w3spaces.com",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://shorts-summary.w3spaces.com'); // Permitir qualquer origem
+  res.header('Access-Control-Allow-Origin', '*'); // Permitir qualquer origem
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
