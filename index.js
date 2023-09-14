fd2b7a8831b434edac182db4fc14cead4b8ff904
Wesley Fernandes <wesleyintelligence@protonmail.com>
2023-09-13 22:16:21 -0300

    new cors config

--- index.js
+++ index.js
@@ -23,9 +23,11 @@ const io = new Server(server, {
   }
 });
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Permitir qualquer origem
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+app.use((request, response, next) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Credentials", "true");
+  response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
